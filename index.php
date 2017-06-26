66d9ba9630a9e52aa91023e7437ca6239edb3ead
Zaki Hussain <m-zaki@live.dk>
2017-06-26 13:31:37 +0200

    CSS update

--- index.php
+++ index.php
@@ -133,7 +133,7 @@
       <div class="row">
         <a class="waves-effect waves-light btn facebook-blue">
         <i class="fa fa-facebook-square left"></i>Log ind med Facebook</a>
-        <a class="waves-effect waves-light btn background-orange">
+        <a class="waves-effect waves-light btn background-orange" href="#modal1">
         <i class="material-icons left">perm_identity</i>Opret bruger</a>
       </div>
     </div>
@@ -183,16 +183,16 @@
               <h2 class="center safemove-orange"><i class="fa fa-heart"></i></h2>
               <h5 class="center safemove-blue">Ratere</h5>
 
-              <p class="light safemove-blue">Lorem Ipsum er ganske enkelt fyldtekst fra print- og typografiindustrien. Lorem Ipsum har været standard fyldtekst siden 1500-tallet, hvor en ukendt trykker sammensatte en tilfældig spalte for at trykke en bog til sammenligning af forskellige skrifttyper. Lorem Ipsum har ikke alene overlevet fem århundreder, men har også vundet indpas i elektronisk typografi uden væsentlige ændringer.</p>
+              <p align="justify" class="light safemove-blue">Du er som rater en essentiel del af Safemove-community. Det er dig, som skal gøre din ekspertise om dit boligområde tilgængelig. Tilgængeligheden af din og dine naboers mening, vil hjælpe søgerene i Safemove-community, til at skabe et retvisende billede af jeres boligområde. Din mening vil derfor hjælpe dine kommende naboer med at vælge det perfekte boligområde for dem. </p>
             </div>
           </div>
 
-          <div class="col s12 m4">
+          <div id="safemove-community-info" class="col s12 m4">
             <div class="icon-block">
               <h2 class="center safemove-orange"><i class="fa fa-info"></i></h2>
               <h5 class="center safemove-blue">Information</h5>
 
-              <p class="light safemove-blue">Lorem Ipsum er ganske enkelt fyldtekst fra print- og typografiindustrien. Lorem Ipsum har været standard fyldtekst siden 1500-tallet, hvor en ukendt trykker sammensatte en tilfældig spalte for at trykke en bog til sammenligning af forskellige skrifttyper. Lorem Ipsum har ikke alene overlevet fem århundreder, men har også vundet indpas i elektronisk typografi uden væsentlige ændringer.</p>
+              <p align="justify" class="light safemove-blue">Den information som du kan hente i Safemove-community er både subjektive og objektive information. Den subjektive information, er den som er konstrueret af vores rater og den objektive information er data, som Safemove har hentet og inkorporeret i kort, således at det bliver nemt og enkelt at se din kommende boligs placering i forhold fx skolemuligheder i området.</p>
             </div>
           </div>
 
@@ -201,7 +201,7 @@
               <h2 class="center safemove-orange"><i class="fa fa-search"></i></h2>
               <h5 class="center safemove-blue">Søgere</h5>
 
-              <p class="light safemove-blue">Lorem Ipsum er ganske enkelt fyldtekst fra print- og typografiindustrien. Lorem Ipsum har været standard fyldtekst siden 1500-tallet, hvor en ukendt trykker sammensatte en tilfældig spalte for at trykke en bog til sammenligning af forskellige skrifttyper. Lorem Ipsum har ikke alene overlevet fem århundreder, men har også vundet indpas i elektronisk typografi uden væsentlige ændringer.</p>
+              <p align="justify" class="light safemove-blue">Du har som en søger i Safemove-community mulighed for, helt gratis, at søge information om dit kommende boligområde. Dette vil give dig bedre indsigt i hvordan det er at bo i det pågældende boligområde og hvordan dit boligområde ligger i forhold til fx skolemuligheder. Du kan dermed finde det boligområde der er det bedste match iforhold til dine personlige præferencer.</p>
             </div>
           </div>
         </div>
