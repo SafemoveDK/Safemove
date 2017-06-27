95ef18961221b3bbdcf52ee83e615e3002a521ab
Zaki Hussain <m-zaki@live.dk>
2017-06-27 09:18:14 +0200

    mail.php igen

--- mail.php
+++ mail.php
@@ -1,11 +1,13 @@
 <?php
-  $name = $_POST['name'];
-  $email = $_POST['email'];
-  $message = $_POST['message'];
-  $formcontent = "Fra: $name \n Besked: $message";
-  $recipient = "m-zak@live.dk";
-  $subject = "Kontaktformular";
-  $mailheader = "Fra: $email \r\n";
-  mail($recipient, $subject, $formcontent, $mailheader) or die("Fejl!");
-  echo "Tak for din besked!";
+  if(isset($_POST)){
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $formcontent = "Fra: $name \n Besked: $message";
+    $recipient = "m-zak@live.dk";
+    $subject = "Kontaktformular";
+    $mailheader = "Fra: $email \r\n";
+    mail($recipient, $subject, $formcontent, $mailheader) or die("Fejl!");
+    echo "Tak for din besked!";
+  }
 ?>
