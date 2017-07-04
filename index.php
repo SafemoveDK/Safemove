7042c7d9f193ee26bc6eff44274db1a4c49c19f1
Zaki Hussain <m-zaki@live.dk>
2017-07-04 19:26:27 +0200

    Inserted contact_form

--- index.php
+++ index.php
@@ -132,7 +132,7 @@
     </div>
 
     <!-- Rate your area section -->
-    <div class="rate-area-login center">
+    <div id="rate-area-login" class="rate-area-login center">
       <br />
       <br />
       <div class="row">
@@ -147,13 +147,13 @@
     <div id="create-user-modal" class="modal modal-fixed-footer">
 
         <div class="modalheader">
-          &nbsp;
+          <br />
           <h4 class="center">Opret bruger</h4>
         </div>
 
         <div id="loginmodal">
           <form id="loginform" name="loginform" method="post" action="index.html">
-            &nbsp;
+            <br />
             <div class="container">
 
               <div class="row">
@@ -229,9 +229,43 @@
     </div>
 
     <div id="kontakt-os" class="background-blue">
-      <div>
-        <p class="contact-headline center-align">Du er altid velkommen til at kontakte os</p>
-        <?php include 'contact_form.php';?>
+      <p class="contact-headline center">Du er altid velkommen til at kontakte os</p>
+      <div class="container">
+
+        <div class="row">
+          <form action="mail.php" method="POST" class="col s12">
+            <div class="row">
+
+              <div class="row">
+                <div class="input-field col s6">
+                  <input  id="name" name="name" type="text" class="validate" required>
+                  <label for="name">Navn</label>
+                </div>
+                <div class="input-field col s6">
+                  <input id="email" name="email" type="text" class="validate" required>
+                  <label for="email">E-mail</label>
+                </div>
+              </div>
+
+              <div class="row">
+                <div class="input-field col s12">
+                  <textarea id="message" name="message" class="materialize-textarea" required></textarea>
+                  <label for="message">Besked til Safemove</label>
+                </div>
+              </div>
+
+              <div class="row">
+                <div class="col m12">
+                  <button class="btn waves-effect waves-light background-orange" type="submit" name="action">Send besked
+                    <i class="material-icons right">send</i>
+                  </button>
+                </div>
+              </div>
+
+            </div>
+          </form>
+        </div>
+
       </div>
     </div>
 
--- rate-area-1.php
+++ rate-area-1.php
@@ -7,15 +7,25 @@
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
       <!--Import materialize.css-->
       <link type="text/css" rel="stylesheet" href="materialize/css/materialize.min.css"  media="screen,projection"/>
-      <link type="text/css" rel="stylesheet" href="style.css"/>
-      <meta charset="iso-8859-1"
-      <!--Let browser know website is optimized for mobile-->
+      <link type="text/css" rel="stylesheet" href="./style.css"/>
+      <meta charset="iso-8859-1" />
+      <!-- Let browser know website is optimized for mobile-->
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-
-      <title></title>
+      <title>Safemove - </title>
     </head>
 
     <body>
-    </body>
 
+      <div class="bedom background-blue">
+        <h3 class="center">Bedøm dit område</h3>
+      </div>
+
+      <div class="container">
+        <br />
+        <p class="safemove-blue">Find din adresse: </p>
+        <br />
+        <p class="safemove-blue">Du har valgt følgende adresse: </p>
+      </div>
+
+    </body>
   </html>
