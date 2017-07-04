8e800b1f5e6e245adba7b9982ffb2b537bc7b41d
Malek Qasem <malekfreestyle@gmail.com>
2017-07-04 16:19:40 +0200

    Created database & connection code

---
+++ openDatabase.php
@@ -0,0 +1,18 @@
+<?php
+
+    
+    require_once ´openDatabase.php´;
+    $connection = mysqli_connect($db_hostname, $db_username, $db_password);
+    
+    if(!$connection)
+    {
+        die("Kan ikke forbinde til MySQL: " . mysqli_connect_error());
+    }
+    
+    $selected_db = mysqli_select_db($connection, $db_database);
+    
+    if(!$selected_db)
+    {
+        die("Kunne ikke vælge database: " . mysqli_connect_error());
+    }
+?>
