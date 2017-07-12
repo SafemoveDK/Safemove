ccaf31d508702fd46e23c43beae06cc3225c71fe
Zaki Hussain <m-zaki@live.dk>
2017-07-12 18:09:53 +0200

    Struture to area-rating.php

--- area-rating.php
+++ area-rating.php
@@ -56,6 +56,51 @@
 
         </div>
 
+        <div class="row background-blue">
+          <div class="col s6 area-overview">
+            <i class="material-icons prefix safemove-orange">location_on</i>
+            <a class="white-text">Oversigt over område</a>
+          </div>
+
+          <div class="col s6 center area-overview">
+          </div>
+        </div>
+
+        <div class="row center">
+
+          <div class="col s6">
+            <p>Der skal være maps her!</p>
+          </div>
+
+          <div class="col s6 left-border">
+            <p>PARAMETER 1</p>
+            <p>PARAMETER 2</p>
+            <p>PARAMETER 3</p>
+            <p>PARAMETER 4</p>
+            <p>PARAMETER 5</p>
+          </div>
+
+        </div>
+
+        <div class="row background-blue">
+          <div class="col s6 area-comment">
+            <i class="material-icons prefix safemove-orange">comment</i>
+            <a class="white-text">Kommentarer</a>
+          </div>
+
+          <div class="col s6 center area-comment">
+          </div>
+        </div>
+
+
+
+
+        <div class="background-blue">
+          &nbsp;
+          <p class="container white-text center-align">© Safemove 2017. Alle rettigheder forbeholdes.</p>
+          &nbsp;
+        </div>
+
       <!--Import jQuery before materialize.js-->
       <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
       <script type="text/javascript" src="materialize/js/materialize.min.js"></script>
