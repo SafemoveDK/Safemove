78cf9f80bd35c8da0aa0bf4ab7a83df86ac196a2
Zaki Hussain <m-zaki@live.dk>
2017-08-29 11:04:46 +0200

    css-fixed

--- index.php
+++ index.php
@@ -89,30 +89,31 @@
       <div class="section">
 
         <div class="row">
-          <div class="col s4 m4">
+
+          <div class="col s4">
             <div class="icon-block">
               <h2 class="center safemove-orange"><i class="fa fa-heart"></i></h2>
               <h5 class="center safemove-blue">Ratere</h5>
 
-              <p align="justify" class="light safemove-blue">Du er som rater en essentiel del af Safemove-community. Det er dig, som skal gøre din ekspertise om dit boligområde tilgængelig. Tilgængeligheden af din og dine naboers mening, vil hjælpe søgerene i Safemove-community, til at skabe et retvisende billede af jeres boligområde. Din mening vil derfor hjælpe dine kommende naboer med at vælge det perfekte boligområde for dem. </p>
+              <p align="center" class="light safemove-blue">Du er som rater en essentiel del af Safemove-community. Det er dig, som skal gøre din ekspertise om dit boligområde tilgængelig. Tilgængeligheden af din og dine naboers mening, vil hjælpe søgerene i Safemove-community, til at skabe et retvisende billede af jeres boligområde. Din mening vil derfor hjælpe dine kommende naboer med at vælge det perfekte boligområde for dem. </p>
             </div>
           </div>
 
-          <div class="col s4 m4" >
+          <div class="col s4" >
             <div class="icon-block">
               <h2 class="center safemove-orange"><i class="fa fa-info"></i></h2>
               <h5 class="center safemove-blue">Information</h5>
 
-              <p align="justify" class="light safemove-blue">Den information som du kan hente i Safemove-community er både subjektive og objektive information. Den subjektive information, er den som er konstrueret af vores rater og den objektive information er data, som Safemove har hentet og inkorporeret i kort, således at det bliver nemt og enkelt at se din kommende boligs placering i forhold fx skolemuligheder i området.</p>
+              <p align="center" class="light safemove-blue">Den information som du kan hente i Safemove-community er både subjektive og objektive information. Den subjektive information, er den som er konstrueret af vores rater og den objektive information er data, som Safemove har hentet og inkorporeret i kort, således at det bliver nemt og enkelt at se din kommende boligs placering i forhold fx skolemuligheder i området.</p>
             </div>
           </div>
 
-          <div class="col s4 m4">
+          <div class="col s4">
             <div class="icon-block">
               <h2 class="center safemove-orange"><i class="fa fa-search"></i></h2>
               <h5 class="center safemove-blue">Søgere</h5>
 
-              <p align="justify" class="light safemove-blue">Du har som en søger i Safemove-community mulighed for, helt gratis, at søge information om dit kommende boligområde. Dette vil give dig bedre indsigt i hvordan det er at bo i det pågældende boligområde og hvordan dit boligområde ligger i forhold til fx skolemuligheder. Du kan dermed finde det boligområde der er det bedste match iforhold til dine personlige præferencer.</p>
+              <p align="center" class="light safemove-blue">Du har som en søger i Safemove-community mulighed for, helt gratis, at søge information om dit kommende boligområde. Dette vil give dig bedre indsigt i hvordan det er at bo i det pågældende boligområde og hvordan dit boligområde ligger i forhold til fx skolemuligheder. Du kan dermed finde det boligområde der er det bedste match iforhold til dine personlige præferencer.</p>
             </div>
           </div>
         </div>
