a3e33d2bffdcb42fa4968e45233fefe4a5ebd04e
Zaki Hussain <m-zaki@live.dk>
2017-09-25 22:09:38 +0200

    nnn

--- header.php
+++ header.php
@@ -2,15 +2,17 @@
   <html>
     <head>
       <!--Import Google Icon Font-->
-      <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <link rel="stylesheet" href="http://fonts.googleapis.com/icon?family=Material+Icons">
+
       <!--Import Font Awesome-->
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-      <!--Import materialize.css-->
 
+      <!--Import materialize.css-->
       <link type="text/css" rel="stylesheet" href="materialize/css/materialize.min.css"  media="screen,projection"/>
       <link type="text/css" rel="stylesheet" href="./style.css"/>
 
       <meta charset="iso-8859-1" />
+
       <!-- Let browser know website is optimized for mobile-->
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
